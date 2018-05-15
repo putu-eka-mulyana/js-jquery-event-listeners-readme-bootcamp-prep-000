8af9aebb62ad94fe89afbdd5d23c6b59903e2023
putu eka mulyana <putuekamulyana97@gmail.com>
2018-05-15 12:22:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,22 @@
 $(document).ready(function(){
 
 // call functions here
+ $('p').on("click", function getIt() {
+    alert("Hey!")
+});
+
+
+  $('input').on('keydown', function pressIt(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+});
+  
+
+  
+    $('img').addClass("tasty");
+$('input').on("keydown", function pressIt(){
+  
+})
 
 });
