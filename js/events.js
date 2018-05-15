832c89ceac3c37907a75911a371d025c935980cc
putu eka mulyana <putuekamulyana97@gmail.com>
2018-05-15 12:25:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,20 @@
 //define functions here
+function getIt() {
+ $('p').on("click", function() {
+    alert("Hey!")
+});
+}
+
+  $('input').on('keydown', function pressIt(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+});
+  
+
+function frameIt(){
+$('img').addClass("tasty");
+}
 
 $(document).ready(function(){
 
